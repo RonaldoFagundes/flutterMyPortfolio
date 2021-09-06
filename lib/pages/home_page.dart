5c645ee57f8c684ff46bcbce226268cded55ca5f
RonaldoFagundes <ronaldofagundes@gmail.com>
2021-09-06 12:52:00 -0300

    adcionar page link

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:my_portfolio/pages/java_page.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
